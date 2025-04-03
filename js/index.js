d7cb8dd748435aa41b23a3e3eb61f220abe19548
usuario <indirasierragalano@yahoo.es>
2025-04-03 10:45:56 +0200

    css to menu secction in html, starting with pink and shine

--- js/index.js
+++ js/index.js
@@ -23,4 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('home').classList.add('active');
 });
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    //click event to all menu links
+    document.querySelectorAll('.menu-li a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            
+            // hide sections
+            document.querySelectorAll('.section').forEach(section => {
+                section.classList.remove('active');
+            });
+            
+            // show active section
+            document.getElementById(targetId).classList.add('active');
+            
+            //smooth Scroll 
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+    
+    // home section active default
+    document.getElementById('home').classList.add('active');
+  
